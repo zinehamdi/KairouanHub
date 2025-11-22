6c8c6b27fe764d750b351974ee58250d0f2bed0e
zine hamdi <zinehamdi8@gmail.com>
2025-11-22 23:17:02 +0100

    Use comprehensive seeders for better demo content

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,24 +16,11 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RolesSeeder::class,
             SuperAdminSeeder::class,
-            CategoriesSeeder::class,
-            ServicesSeeder::class,
+            ComprehensiveCategorySeeder::class,
+            ComprehensiveServiceSeeder::class,
         ]);
 
         // Optionally seed a couple of normal users
         User::factory()->count(2)->create();
     }
 }
-
-        
-                // Seed sample providers (EN: demo providers; AR: مزودون تجريبيون)
-                if (class_exists(\App\Models\Service::class)) {
-                    $services = \App\Models\Service::query()->inRandomOrder()->take(5)->get();
-                    \App\Models\ProviderProfile::factory()->count(2)->approved()->create()->each(function($profile) use ($services) {
-                        $attach = [];
-                        foreach ($services->random(min(2, $services->count())) as $svc) {
-                            $attach[$svc->id] = ['price_min' => rand(20,80), 'price_max' => rand(90,200)];
-                        }
-                        $profile->services()->attach($attach);
-                    });
-                }
